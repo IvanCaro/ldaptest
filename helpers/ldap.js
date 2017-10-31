3f90448c96edd8c17373817b3d55ee94aecb3737
Iván Caro <ivanc@lagash.com>
2017-10-31 14:53:53 -0300

    add test

---
+++ helpers/ldap.js
@@ -0,0 +1,79 @@
+const ActiveDirectory = require('activedirectory');
+
+const configExternos = {
+  url: process.env.LDAP_URI_EXTERNOS,
+  baseDN: process.env.LDAP_USER_ADMIN_EXTERNOS,
+  tlsOptions: {
+    rejectUnauthorized: (process.env.LDAP_VERIFICAR_CERTIFICADO_TLS === "true")
+  }
+}
+
+const configInternos = {
+  url: process.env.LDAP_URI_INTERNOS,
+  baseDN: process.env.LDAP_BASE_URI_INTERNOS
+}
+
+const activeDirectoryExternos = new ActiveDirectory(configExternos);
+const activeDirectoryInternos = new ActiveDirectory(configInternos);
+
+let reintent = 0;
+
+const autenticateProcess = (userName, password) => new Promise((resolve, reject) => {
+  const ad = userName.startsWith("e_") ? activeDirectoryExternos : activeDirectoryInternos;
+
+  ad.authenticate(userName, password, (err, auth) => {
+    if (err && reintent < 5) {
+      reintent += 1;
+      autenticateProcess(userName, password);
+    }
+    if (err && reintent >= 5) {
+      reintent = 0;
+      reject(err);
+    }
+    if (auth) {
+      reintent = 0;
+      resolve(auth);
+    } else {
+      reintent = 6;
+      reject('Authentication failed!');
+    }
+  });
+});
+
+exports.authenticate = autenticateProcess;
+
+exports.extractDomainAndUserName = (userName) => {
+  if (!userName)
+    return Promise.reject('parametro requerido [userName]');
+
+  return new Promise((resolve, reject) => {
+    try {
+      const userNameAux = userName.toString();
+      const domainUser = {};
+      const domain = userName.startsWith("e_") ?
+                     process.env.DEFAULT_DOMAIN_EXTERNOS
+                     : process.env.DEFAULT_DOMAIN_INTERNOS;
+
+      let separator;
+      if (userNameAux.indexOf("@") !== -1)
+        separator = "@";
+      else if (userNameAux.indexOf("\\") !== -1)
+        separator = "\\";
+
+      if (separator) {
+        const userNameParts = userNameAux.split(separator);
+        domainUser.User = userNameParts[0];
+        domainUser.Domain = domain;
+
+        return resolve(domainUser);
+      }
+
+      domainUser.User = userNameAux;
+      domainUser.Domain = domain;
+
+      return resolve(domainUser);
+    } catch (err) {
+      return reject(err);
+    }
+  });
+}
---
+++ test/ldapjs.js
@@ -0,0 +1,56 @@
+/* eslint-env node, mocha */
+require('dotenv').config({
+  path: 'env.env'
+});
+
+const {
+    assert
+  } = require('chai');
+const ldap = require('../helpers/ldap');
+
+  // const userName = "jtorresr@minvu.cl";
+  // const password = 'lagash.2017';
+
+const userName = 'e_15617277-4@minvuext.cl';
+const password = 'password.1';
+
+let authResult;
+before(done => {
+  ldap
+    .authenticate(userName, password)
+      .then(result => {
+        assert.isNotNull(result);
+        authResult = result;
+        done();
+      })
+    .catch(err => {
+      done(err);
+    });
+});
+
+describe.only('Autenticando usuario con active directory (ldap)', () => {
+  it('Usuario autenticado', () => {
+    assert.isNotNull(authResult);
+  });
+});
+
+let domainUser;
+before(done => {
+  ldap
+    .extractDomainAndUserName(userName)
+      .then(result => {
+        assert.isNotNull(result);
+        domainUser = result;
+        done();
+      })
+    .catch(err => {
+      done(err);
+    });
+});
+
+describe('Extrayendo usuario y dominio desde userName', () => {
+  it('Usuario y dominio', () => {
+    assert.isNotNull(domainUser);
+  });
+});
+

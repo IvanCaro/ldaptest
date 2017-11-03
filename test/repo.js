c515d0618a98335370a53b4dba92eaa092503ab9
Iván Caro <ivanc@lagash.com>
2017-11-03 15:25:31 -0300

    se agrega repo to bd

--- test/repo.js
+++ test/repo.js
@@ -9,8 +9,21 @@ const config = {
   user: process.env.DB_USER_NAME,
   password: process.env.DB_USER_PASS,
   server: process.env.DB_SERVER,
-  database: process.env.DB_NAME
+  // host: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
+  trustedConnection: true,
+  encrypt: true,
+  port: process.env.DB_PORT,
+  dialect: 'mssql',
+  dialectOptions: {
+    database: process.env.DB_NAME,
+    instanceName: 'Instance2',
+    trustedConnection: true
+  }
 };
+
+// console.log(config);
+// console.log('\n');
 
 const connect = cb => {
   sql.connect(config, err => {
@@ -28,7 +41,7 @@ function getUserById(userId) {
     return Promise.reject('parametro requerido [userId]');
   }
 
-  const sqlUsuario = 'SELECT TOP 1 * FROM [dbo].[Usuario] ';
+  const sqlUsuario = 'SELECT TOP 1 * FROM dbo.Usuario';
 
   return new Promise((resolve, reject) => {
     new sql.Request()
